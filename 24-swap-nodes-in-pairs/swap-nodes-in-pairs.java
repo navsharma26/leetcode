91b770a308b2cb1c9c1ea83ce6f6fd69f77e5825
navsharma26 <navneet30405@gmail.com>
2025-11-14 00:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (6.20%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead; // connect odd list with even list
+        return head;
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first; // move to next pair
+        }
+
+        return dummy.next;
+    }
+}
