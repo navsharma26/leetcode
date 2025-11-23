87a54fa27030c354a0dcf5927123d6de61d0e5d6
navsharma26 <navneet30405@gmail.com>
2025-11-23 23:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (26.69%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -8,49 +8,46 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode temp = head;
-        ListNode pre = null;
-        ListNode next = null;
-
-        while (temp != null) {
-            next = temp.next;
-            temp.next = pre;
-            pre = temp;
-            temp = next;
+ class Solution{
+    public void reverselist(ListNode head){
+        ListNode curr=head;
+        ListNode pre=null;
+        ListNode Next=null;
+        while(curr!=null){
+            Next=curr.next;
+            curr.next=pre;
+            pre=curr;
+            curr=Next;
         }
-        return pre;
+         
     }
 
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null || head.next == null || left == right) return head;
-
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-
-        ListNode a = dummy;  
-        for (int i = 1; i < left; i++) {
-            a = a.next;       
+    public ListNode reverseBetween(ListNode head,int left,int right){
+        if(head.next==null||left==right) return head;
+        ListNode a=null;
+        ListNode b=null;
+        ListNode c=null;
+        ListNode d=null;
+        int pos=1;
+        ListNode temp=head;
+        while(temp!=null){
+            if(pos==left-1) a=temp;
+              if(pos==left) b=temp;
+                if(pos==right) c=temp;
+                  if(pos==right+1) d=temp;
+                  temp=temp.next;
+                  pos++;
+                  
         }
+         if(a!=null) a.next=null;
+           if(c!=null)       c.next=null;
+                  reverselist(b);
+                if(a!=null)  a.next=c;
+                  b.next=d;
+                  if(a==null) return c;
+                  return head;
 
-        ListNode b = a.next;  
-        ListNode c = b;       
-        for (int i = left; i < right; i++) {
-            c = c.next;
-        }
+ }
 
-        ListNode d = c.next;   
-
-        a.next = null;
-        c.next = null;
-
-        ListNode newHead = reverseList(b);
-
-        a.next = newHead;
-
-        b.next = d;
-
-        return dummy.next;
-    }
-}
+ }
+ 
