8164d656c38e09abada80be576479c473820d5c4
NAVNEET SHARMA <navneet30405@gmail.com>
2026-04-03 19:07:26 +0530

    Time: 28 ms (51.95%) | Memory: 81.5 MB (10.57%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,24 +1,36 @@
 class Solution {
+
+     boolean canDistribute(int[] quantities, int n, int mid) {
+    int stores = 0;
+
+    for (int i = 0; i < quantities.length; i++) {
+        stores += (quantities[i] + mid - 1) / mid;
+    }
+
+    if (stores <= n) {
+        return true;
+    } else {
+        return false;
+    }
+     }
+
     public int minimizedMaximum(int n, int[] quantities) {
-        int left = 1;
-        int right = 100000;
+        int left=1;
+        int right=java.util.Arrays.stream(quantities).max().getAsInt();
+        int result=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(canDistribute( quantities,  n,  mid)){
+                result=mid;
+                right=mid-1;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            int store = 0; // reset every iteration
-
-            for (int i = 0; i < quantities.length; i++) {
-                store += (quantities[i] + mid - 1) / mid;
             }
-
-            if (store <= n) {
-                right = mid;
-            } else {
-                left = mid + 1;
+            else{
+                left=mid+1;
             }
+        
         }
-
-        return left;
+        return result;
+        
     }
 }
