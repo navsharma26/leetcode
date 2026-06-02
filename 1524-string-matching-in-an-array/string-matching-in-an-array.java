d8a91cab3b866a85475a0138715a00878e92119f
NAVNEET SHARMA <navneet30405@gmail.com>
2026-06-02 18:16:11 +0530

    Time: 4 ms (96.29%) | Memory: 43.7 MB (64.40%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,15 +1,17 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        List<String> result = new ArrayList<>();
+         List<String>ans=new ArrayList<>();
+       for(int i=0;i<words.length;i++){
+        for(int j=0;j< words.length;j++){
+            if(i!=j&& words[j].contains(words[i])){
+                ans.add(words[i]);
+            break;
 
-        for (int i = 0; i < words.length; i++) {
-            for (int j = 0; j < words.length; j++) {
-                if (i != j && words[j].contains(words[i])) {
-                    result.add(words[i]);
-                    break;
-                }
             }
+            
         }
-        return result;
+       }
+       return ans;
+        
     }
 }
